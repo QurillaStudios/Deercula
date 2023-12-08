86a9d183ac49b1fa88ab6a45b113f4d0aadb0cf5
QurillaStudios <113723158+QurillaStudios@users.noreply.github.com>
2023-12-08 20:46:33 +0100

    Deercula Stuff

--- DeerculaGJ/Assets/Thomas/Deercula.cs
+++ DeerculaGJ/Assets/Thomas/Deercula.cs
@@ -6,6 +6,15 @@ public class Deercula : MonoBehaviour
 {
     [SerializeField]
     private int initialHealth;
+    [SerializeField] private float attackCoolDownTime = 1f;
+    [SerializeField]
+    private LayerMask bitableLayerMask;
+    [SerializeField]
+    private GameObject BiteIndicator;
+
+    private float currentAttackCoolDownTimer = 0f;
+    private bool canAttack = true;
+
     private int health;
     Rigidbody2D body;
 
@@ -14,16 +23,35 @@ public class Deercula : MonoBehaviour
 
     public float runSpeed = 20.0f;
 
+    private Vector3 lastMovementDirection;
+
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
         health = initialHealth;
+        BiteIndicator.SetActive(false);
     }
 
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            OnAttackTriggered();
+        }
+
+        if(new Vector2(horizontal, vertical) != Vector2.zero)
+        {
+            lastMovementDirection = new Vector2(horizontal, vertical);
+            if( horizontal != 0f && vertical != 0f )
+            {
+                lastMovementDirection = lastMovementDirection / 1.5f;
+            }
+        }
+
+        Debug.Log("LastMovementDirection: " + lastMovementDirection);
     }
 
     private void FixedUpdate()
@@ -31,9 +59,56 @@ public class Deercula : MonoBehaviour
         body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
     }
 
+
+
+    private void OnAttackTriggered()
+    {
+        if(canAttack)
+        {
+        Debug.Log("Attack triggered");
+            Attack();
+            StartCoroutine(AttackCooldown());
+        }
+    }
+
+    private void Attack()
+    {
+        Vector2 attackCirclePosition = transform.position + lastMovementDirection;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(attackCirclePosition, 1f, bitableLayerMask);
+
+        foreach(Collider2D collider in colliders)
+        {
+            if( collider.gameObject.tag == "Bitable")
+            {
+                //collider.gameObject.GetComponent<Animal>().TakeDamage();
+            }
+        }
+
+        StartCoroutine(ShowBiteIndicator(attackCirclePosition));
+    }
+
+    private IEnumerator AttackCooldown()
+    {
+        canAttack= false;
+        yield return new WaitForSeconds(attackCoolDownTime);
+        canAttack= true;
+    }
+
+    private IEnumerator ShowBiteIndicator(Vector2 position)
+    {
+        BiteIndicator.transform.position = position;
+        BiteIndicator.SetActive(true);
+        yield return new WaitForSeconds(0.2f);
+        BiteIndicator.SetActive(false);
+    }
+
     public void TakeDamage()
     {
         initialHealth -= 1;
         Debug.Log("Damage taken");
+        if (health <= 0)
+        {
+            Debug.Log("GameOver");
+        }
     }
 }
