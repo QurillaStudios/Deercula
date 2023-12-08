88879d611a8d38f728adbc4e5c4cb33823cd0e42
QurillaStudios <113723158+QurillaStudios@users.noreply.github.com>
2023-12-08 17:28:00 +0100

    TestCommit

---
+++ DeerculaGJ/Assets/Thomas/Deercula.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deercula : MonoBehaviour
+{
+    public float speed = 100;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 tempVect = new Vector3(h, v, 0);
+        tempVect = tempVect.normalized * speed * Time.deltaTime;
+        rb.MovePosition(rb.transform.position + tempVect);
+    }
+}
