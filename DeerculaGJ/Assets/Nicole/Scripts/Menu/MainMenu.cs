1c1fd25c376474f24168cb8e9dbf302e9c0b6c5e
QurillaStudios <113723158+QurillaStudios@users.noreply.github.com>
2023-12-10 09:55:16 +0100

    Added Video Player

--- DeerculaGJ/Assets/Nicole/Scripts/Menu/MainMenu.cs
+++ DeerculaGJ/Assets/Nicole/Scripts/Menu/MainMenu.cs
@@ -2,11 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class MainMenu : MonoBehaviour
 {
+    public VideoPlayer videoPlayer;
+    [SerializeField] private GameObject videoImage;
+
     public void StartGame()
     {
+        StartCoroutine(StartVideoThenGame());
+        
+    }
+
+
+    private IEnumerator StartVideoThenGame()
+    {
+        videoImage.SetActive(true);
+        videoPlayer.Play();
+
+        yield return new WaitForSeconds(6.5f);
+
         SceneManager.LoadScene("Game");
     }
 
---
+++ DeerculaGJ/Assets/Thomas/MenuManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class MenuManager : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
